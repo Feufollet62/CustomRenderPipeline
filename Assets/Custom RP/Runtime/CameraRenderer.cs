1723a24a851f61a681fa5192644b551875f3408e
Eliot <fromenteliot@gmail.com>
2023-03-13 13:13:14 +0100

    Added camera clear flags

--- Assets/Custom RP/Runtime/CameraRenderer.cs
+++ Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -33,7 +33,16 @@ public partial class CameraRenderer
     private void Setup()
     {
         _context.SetupCameraProperties(_camera);
-        _buffer.ClearRenderTarget(true, true, Color.clear);
+        
+        CameraClearFlags flags = _camera.clearFlags;
+        
+        _buffer.ClearRenderTarget
+        (
+            flags <= CameraClearFlags.Depth, 
+            flags == CameraClearFlags.Color, 
+            flags == CameraClearFlags.Color ? _camera.backgroundColor.linear : Color.clear
+        );
+        
         _buffer.BeginSample(SampleName);
         ExecuteBuffer();
     }
