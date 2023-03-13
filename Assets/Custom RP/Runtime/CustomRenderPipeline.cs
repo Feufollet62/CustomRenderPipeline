e9ae4c02b64ea10cc45148c415eba31e7a8cdd6d
Eliot <fromenteliot@gmail.com>
2023-03-13 10:15:41 +0100

    Drawing skybox

--- Assets/Custom RP/Runtime/CustomRenderPipeline.cs
+++ Assets/Custom RP/Runtime/CustomRenderPipeline.cs
@@ -3,8 +3,37 @@ using UnityEngine.Rendering;
 
 public class CustomRenderPipeline : RenderPipeline
 {
+    private CameraRenderer _renderer = new CameraRenderer();
+    
     protected override void Render(ScriptableRenderContext context, Camera[] cameras)
     {
-        return;
+        foreach (Camera cam in cameras)
+        {
+            _renderer.Render(context,cam);
+        }
+    }
+}
+
+public class CameraRenderer
+{
+    private ScriptableRenderContext _context;
+    private Camera _camera;
+
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        _context = context;
+        _camera = camera;
+        
+        DrawVisibleGeometry();
+        Submit();
+    }
+
+    private void DrawVisibleGeometry () {
+        _context.DrawSkybox(_camera);
+    }
+    
+    private void Submit()
+    {
+        _context.Submit();
     }
 }
