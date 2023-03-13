6a97a7a836793fb97dcc7f23914a7ee3dd8af244
Eliot <fromenteliot@gmail.com>
2023-03-13 15:27:43 +0100

    GPU Instancing

---
+++ Assets/Custom RP/Examples/PerObjectMaterialProperties.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class PerObjectMaterialProperties : MonoBehaviour
+{
+	static int _baseColorId = Shader.PropertyToID("_BaseColor");
+	[SerializeField] Color baseColor = Color.white;
+	static MaterialPropertyBlock block;
+	
+	private void Awake()
+	{
+		OnValidate();
+	}
+	
+	private void OnValidate()
+	{
+		if (block == null) block = new MaterialPropertyBlock();
+		block.SetColor(_baseColorId, baseColor);
+		
+		GetComponent<Renderer>().SetPropertyBlock(block);
+	}
+}
