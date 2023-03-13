d0b867bf3302ad7516cfd07d41b6fdd3dda88d88
Eliot <fromenteliot@gmail.com>
2023-03-13 14:55:36 +0100

    Working unlit shader

--- Assets/Custom RP/ShaderLibrary/Common.hlsl
+++ Assets/Custom RP/ShaderLibrary/Common.hlsl
@@ -7,10 +7,8 @@
 #define UNITY_MATRIX_VP unity_MatrixVP
 #define UNITY_MATRIX_P glstate_matrix_projection
 
-
 #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
 #include "UnityInput.hlsl"
-
 #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/SpaceTransforms.hlsl"
 	
 #endif
